fd781915a34bc2f95fdc022e16a7c51fd2d341f0
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-09-07 22:10:54 -0600

    EJERCICIO 2

---
+++ SEMANA 6 EJERCICIO 2/SEMANA 6 EJERCICIO 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+
+int[] Score = { 90, 71, 82, 93, 75, 82 };
+
+var LQ = from L in Score where L > 80 
+         orderby L descending 
+         select L;
+
+foreach (var L in LQ)
+{
+    Console.WriteLine(L.ToString);
+
+}
+
+Console.WriteLine("Ing.Gutierrez");
+
