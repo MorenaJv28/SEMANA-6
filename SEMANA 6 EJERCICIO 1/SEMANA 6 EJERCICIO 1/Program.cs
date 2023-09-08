089161b1c253a11bf7e9d1f31e730e09e7ee7757
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-09-07 21:35:49 -0600

    EJERCICIO 1

---
+++ SEMANA 6 EJERCICIO 1/SEMANA 6 EJERCICIO 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+
+int[] scores = { 97, 92, 81, 60 };
+
+var L =  from LQ in scores where LQ > 80 select LQ;
+
+foreach (var l in L)
+{
+    Console.Write(l.ToString() + " ");
+}
+
